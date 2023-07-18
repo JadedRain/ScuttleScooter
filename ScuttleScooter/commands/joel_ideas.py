e2f55218df95ec63893110f682fa5f388d57ca41
JadedRain <darkenedninja450@gmail.com>
2023-07-18 12:37:14 -0600

    Added the ability to filter sushi based on ingredients

--- ScuttleScooter/commands/joel_ideas.py
+++ ScuttleScooter/commands/joel_ideas.py
@@ -1,3 +1,4 @@
+from tkinter import Menu
 import discord
 from discord.ui import view
 from discord.ext import commands
@@ -14,14 +15,27 @@ class JoelCommands(commands.Cog):
     async def on_ready(self):
         print("Joel commands loaded")
 
-    @commands.command(name="sr")
-    async def select_roll(self, ctx):
+    @commands.command(name="shos")
+    async def show_rolls(self, ctx):
         await ctx.channel.send(embed=self.sushi_picker.sushi_info.menu[0], view=self.menu_view)
 
-    @commands.command(name="sl")
-    async def show_roll(self, ctx, *roll_name):
+    @commands.command(name="sels")
+    async def select_roll(self, ctx, *roll_name):
         await ctx.channel.send(self.sushi_picker.select_roll(' '.join(roll_name[:]).title()))
 
+    @commands.command(name="fils")
+    async def filter_roll(self, ctx, *ingredient):
+        ing = ' '.join(ingredient).lower()
+        await ctx.channel.send(f"Now filtering by {ing}")
+        self.sushi_picker.sushi_info.create_menu(ing)
+        self.menu_view = MenuView(self.sushi_picker.sushi_info.menu)
+        await self.show_rolls(ctx)
+    
+    @commands.command(name="clrf")
+    async def clear_filters(self, ctx):
+        self.sushi_picker.sushi_info.create_menu()
+        self.menu_view = MenuView(self.sushi_picker.sushi_info.menu)
+        await ctx.channel.send("Filters cleared")
 async def setup(bot):
     await bot.add_cog(JoelCommands(bot))
 
--- ScuttleScooter/pclasses/sushi_info.py
+++ ScuttleScooter/pclasses/sushi_info.py
@@ -1,6 +1,6 @@
+from audioop import add
 import discord
 import discord.ui
-import math
 import pandas as pd
 from pclasses.sushi_roll import SushiRoll
 
@@ -14,22 +14,36 @@ class SushiInfo():
     def __init__(self):
         self.sushi_file_data = pd.read_csv('./sushi_list.csv')
         self.load_data_to_dict()
-        self.menu = self.create_menu()
+        self.create_menu()
 
     def load_data_to_dict(self):
         for i, roll in enumerate(self.sushi_file_data.iloc):
             self.roll_container[roll[0]] = SushiRoll(*roll)   
 
-    def create_menu(self):
+    def create_menu(self, filter=None):
         menu_pages = []
-        embed = discord.Embed(title="Sushi Menu", description="A list of sushi with their ingredients", color=0xffb19a)
-        for i, roll in enumerate(self.roll_container):
-            if i % 10 == 0 and i != 0:
-                embed.set_footer(text=f"Page {math.ceil(i/10)}")
-                menu_pages.append(embed)
-                embed =  discord.Embed(title="Sushi Menu", description="A list of sushi with their ingredients", color=0xffb19a)
-            embed.add_field(name=f"{self.roll_container[roll].name} ${self.roll_container[roll].price}", value=f"{', '.join(self.roll_container[roll].ingredients)} topped w/ {', '.join(self.roll_container[roll].toppings)}", inline=False)
+        embed = discord.Embed()
 
-        embed.set_footer(text=f"Page {math.ceil(i/10)}")
-        menu_pages.append(embed)
-        return menu_pages
+        def add_page():
+            embed.title = "Sushi Menu"
+            embed.description = "A list of sushi rolls with their ingredients"
+            embed.color = 0xffb19a
+            embed.set_footer(text=f"Page {len(menu_pages) + 1}")
+            menu_pages.append(embed)
+
+        for i, roll in enumerate(self.roll_container):
+            if filter:
+                if filter in self.roll_container[roll].ingredients:
+                    embed.add_field(name=f"{self.roll_container[roll].name} ${self.roll_container[roll].price}", value=f"{', '.join(self.roll_container[roll].ingredients)} topped w/ {', '.join(self.roll_container[roll].toppings)}", inline=False)
+            else:
+                embed.add_field(name=f"{self.roll_container[roll].name} ${self.roll_container[roll].price}", value=f"{', '.join(self.roll_container[roll].ingredients)} topped w/ {', '.join(self.roll_container[roll].toppings)}", inline=False)
+            
+            if len(embed.fields) == 10:
+                add_page()
+                embed = discord.Embed()
+        
+        if len(embed.fields):
+            add_page()
+        self.menu = menu_pages
+
+  

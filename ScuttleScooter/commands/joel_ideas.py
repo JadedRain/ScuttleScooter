b10bffa07fcdc12e1f4c49760ab5397468e678aa
JadedRain <darkenedninja450@gmail.com>
2023-07-20 13:43:35 -0600

    Added a cart to add sushi rolls to

--- ScuttleScooter/commands/joel_ideas.py
+++ ScuttleScooter/commands/joel_ideas.py
@@ -4,12 +4,14 @@ from discord.ui import view
 from discord.ext import commands
 from pclasses.sushi_picker import SushiPicker
 from pclasses.menu_view import MenuView
+from pclasses.sushi_cart import SushiCart
 
 class JoelCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.sushi_picker = SushiPicker()
         self.menu_view = MenuView(self.sushi_picker.sushi_info.menu)
+        self.sushi_cart = SushiCart()
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -36,6 +38,32 @@ class JoelCommands(commands.Cog):
         self.sushi_picker.sushi_info.create_menu()
         self.menu_view = MenuView(self.sushi_picker.sushi_info.menu)
         await ctx.channel.send("Filters cleared")
+
+    @commands.command(name="adds")
+    async def add_item_to_cart(self, ctx, *roll_name):
+        try:
+            self.sushi_cart.add_item_to_cart(self.sushi_picker.sushi_info.roll_container[' '.join(roll_name[:]).title()])
+            await ctx.channel.send(f"Added {' '.join(roll_name[:]).title()} roll to cart.")
+        except:
+            await ctx.channel.send("Couldn't add roll to cart")
+    
+    @commands.command(name="rems")
+    async def remove_item_from_cart(self, ctx, *roll_name):
+        try:
+            self.sushi_cart.remove_item_from_cart(self.sushi_picker.sushi_info.roll_container[' '.join(roll_name[:]).title()])
+            await ctx.channel.send(f"Removed {' '.join(roll_name[:]).title()} roll from cart.")
+        except:
+            await ctx.channel.send("Couldn't remove roll from cart")
+
+    @commands.command(name="carp")
+    async def get_price(self, ctx):
+        await ctx.channel.send(f"Cart total is ${self.sushi_cart.get_cart_total()}")
+
+    @commands.command(name="clrc")
+    async def clear_cart(self, ctx):
+        self.sushi_cart.clear_cart()
+        await ctx.channel.send("Your cart has been cleared.")
+
 async def setup(bot):
     await bot.add_cog(JoelCommands(bot))
 
--- ScuttleScooter/pclasses/sushi_cart.py
+++ ScuttleScooter/pclasses/sushi_cart.py
@@ -1,6 +1,18 @@
-class my_class(object):
-    pass
+class SushiCart():
+    cart_total = 0
+    items_in_cart = []
 
+    def add_item_to_cart(self, item):
+        self.cart_total+=item.price
+        self.items_in_cart.append(item)
 
+    def remove_item_from_cart(self, item):
+        self.items_in_cart.remove(item)
+        self.cart_total-=item.price
+    
+    def clear_cart(self):
+        self.items_in_cart = []
+        self.cart_total = 0
 
-
+    def get_cart_total(self):
+        return self.cart_total
